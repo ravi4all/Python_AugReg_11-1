ded7fbaafb403981a92505926ab2fb88bcd5cca1
ravi4all <ravikanttyagi.2295@gmail.com>
2017-09-11 12:01:00 +0530

    Add files via upload

--- CorePython/FileHandling/KBC_demo.py
+++ CorePython/FileHandling/KBC_demo.py
@@ -1,17 +1,23 @@
-opt1 = ['Yogi', 'Modi', 'Arvind', 'Lalu']
-opt2 = ['Java Virtual Machine', 'Javascript VM', 'Java Visited Machine', 'Java Virtual Model']
+# opt1 = ['Yogi', 'Modi', 'Arvind', 'Lalu']
+# opt2 = ['Java Virtual Machine', 'Javascript VM', 'Java Visited Machine', 'Java Virtual Model']
+ans = ['Modi','Java Virtual Machine']
+money = [1000,2000,5000,10000,20000,40000,80000,160000,10000000,70000000]
+opt = [
+    ['Yogi', 'Modi', 'Arvind', 'Lalu'],
+    ['Java Virtual Machine', 'Javascript VM', 'Java Visited Machine', 'Java Virtual Model']
+       ]
 with open('ques.txt', 'r+') as file:
     while True:
-        a = file.readline()
-        print(a)
-        print("""
-a. Yogi             b. Modi
-c. Arvind           d. Lalu
-        """)
-        ans = input("ans: ")
-        if ans == 'Modi':
-            print("Sahi Jawab...")
-        else:
-            print("Sorry...Try next time...")
-            break
-
+        for i in range(2):
+            a = file.readline()
+            print(a)
+            print("""
+Options - {}
+            """.format(opt[i]))
+            user_ans = input("ans: ")
+            if user_ans in ans:
+                print("Sahi Jawab...")
+            else:
+                print("Sorry...Try next time...")
+                break
+        break
